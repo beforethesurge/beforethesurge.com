b63399ad1f13e6739972101f5777f6e9524ed117
beforethesurge <beforethesurge@gmail.com>
2025-08-15 01:33:36 +0000

    Added scripts for a makeshift pipeline for now

---
+++ dispatcher.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+read -p "DEV or PROD? (D/p): " user_choice
+user_choice=$(echo "$user_choice" | tr "[:upper:]" "[:lower:]")
+if [[ "$user_choice" == "d" || "$user_choice" == "" ]]; then
+    ./scripts/build-site-dev.sh
+elif [[ "$user_choice" == "p" ]]; then
+    ./scripts/build-site-prod.sh
+    ./scripts/build-container.sh
+fi
--- scripts/build-site-prod.sh
+++ scripts/build-site-prod.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 JEKYLL_ENV=production bundle exec jekyll b
-cp -r _site/ ../website/
